20d09ff977dc4594d8084e14447c1bbb5d49c858
d3mchenko <maksim-ignatyuk@mail.ru>
2021-06-25 16:38:36 +0300

    update headerReducer

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -5,13 +5,14 @@ const { default: axios } = require('axios');
 
 export function getBackgroundImageHeader() {
   return (dispatch: Function) => {
-    axios.get('https://api.pexels.com/v1/search?query=sea&orientation=landscape&size=medium&per_page=20&page=1', {
+    axios.get('https://api.pexels.com/v1/search?query=sea&orientation=landscape&size=medium&per_page=25&page=1', {
       headers: {
         Authorization:
           '563492ad6f917000010000014640aabb4e9d420cbe1c0df7daf4c2bf',
       },
     }).then((response: any) => {
       const result = response.data;
+      console.log(result);
       dispatch({type: LOAD_IMAGE_HEADER, payload: result})
     })
   }
--- src/redux/headerReducer.ts
+++ src/redux/headerReducer.ts
@@ -15,12 +15,12 @@ interface InitialHeaderStateTypes {
   export function headerReducer(state: InitialHeaderStateTypes = initialState, action: { type: string; payload?: any }) {
     switch (action.type) {
       case LOAD_IMAGE_HEADER:
-        const imageNumber = ~~(Math.random() * 20);
+        const headerImgNumber = Math.floor(Math.random() * 25); 
         return {
           ...state,
-          headerImage: `${action.payload.photos[imageNumber].src.original}?auto=compress&crop=focalpoint&cs=tinysrgb&dpr=2&h=800&w=1400`,
-          photograph: action.payload.photos[imageNumber].photographer,
-          photographLink: action.payload.photos[imageNumber].url,
+          headerImage: `${action.payload.photos[headerImgNumber].src.original}?auto=compress&crop=focalpoint&cs=tinysrgb&dpr=2&h=800&w=1400`,
+          photograph: action.payload.photos[headerImgNumber].photographer,
+          photographLink: action.payload.photos[headerImgNumber].url,
         };
       default:
         return state;
